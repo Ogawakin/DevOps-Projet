6dc4684923a025b546fe813ce68995cb51d628b3
Timothee <timothee.rognon@etu.univ-grenoble-alpes.fr>
2023-04-05 09:50:29 +0200

    build: creation of the Maven project

---
+++ panda/src/main/java/devop_project/panda/App.java
@@ -0,0 +1,13 @@
+package devop_project.panda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World of Panda!" );
+    }
+}
