4e7c7a5269c1c6e7dc9e8f5d2d6a377f90f078af
bastamar <marion.bastard-rosset@etu.univ-grenoble-alpes.fr>
2023-04-25 14:34:32 +0200

    feat : add all the method and constuctor need (no completed)

--- panda/src/main/java/devop_project/panda/DataFrame.java
+++ panda/src/main/java/devop_project/panda/DataFrame.java
@@ -31,6 +31,18 @@ public class DataFrame {
 	}
 	
 	/**
+	 * create a DataFrame starting from a csv file
+	 * @param file_name
+	 */
+	public DataFrame(String file_name) {
+		this ();
+		
+		
+		
+		
+	}
+	
+	/**
 	 * 
 	 * @param label
 	 * @return the column or null if it doesn't exist
@@ -43,13 +55,131 @@ public class DataFrame {
 		return dataframe.get(label);
 	}
 	
-	public void print() {
-		String acc = "";
-		for(String lbl : dataframe.keySet()) {
-			acc += "[ ";
-			acc += dataframe.get(lbl).toString();
-			acc += "]\n";
+	/**
+	 * 
+	 * @param way 
+	 * @param n
+	 * @return 	the dataframe if way = "all"
+	 * 			the n first lines if way = "first"
+	 * 			the n last lines if way = "last"
+	 */
+	public String toString(String way, int n) {
+		String tab = "";
+		switch (way) {
+		case "all" :
+			for(String lbl : dataframe.keySet()) {
+				tab += "[ ";
+				tab += dataframe.get(lbl).toString();
+				tab += "]\n";
+			}
+			break;
+		case "first" :
+			
+			break;
+		case "last" :
+			
+			break;
 		}
+		return tab; 
+	}
+	
+	/**
+	 * Print all the dataframe
+	 */
+	public void print() {
+		String acc = toString("all", 0);
 		System.out.print(acc);
 	}
+	
+	/**
+	 * Print the first 5 lines of the dataframe
+	 */
+	public void printStart() {
+		String acc = toString("first", 5);
+		System.out.print(acc);
+	}
+	
+	/**
+	 * Print the 5 last lines of the dataframe
+	 */
+	public void printEnd() {
+		String acc = toString("last", 5);
+		System.out.print(acc);
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 * @return a dataframe of the lines wanted
+	 */
+	public DataFrame selectLine(int ...index) {
+		DataFrame sous_dataframe = this;
+		
+		
+		
+		return sous_dataframe;
+	}
+	
+	/**
+	 * 
+	 * @param labels
+	 * @return a dataframe of the column want
+	 */
+	public DataFrame selectColomn(String ...labels) {
+		DataFrame sous_dataframe = this;
+		
+		
+		
+		return sous_dataframe;
+	}
+	
+	/**
+	 * 
+	 * @param label
+	 * @param val
+	 * @return a datafram where all the valeur at the label are val
+	 */
+	public DataFrame selectWhere(String label, Object val) {
+		DataFrame sous_dataframe = this;
+		
+		
+		
+		return sous_dataframe;
+	}
+	
+	/**
+	 * 
+	 * @param label
+	 * @return the val max on the column
+	 */
+	public int max(String label) {
+		int max = 0;
+		
+		return max;
+	}
+	
+	/**
+	 * 
+	 * @param label
+	 * @return the val min on the column
+	 */
+	public int min(String label) {
+		int min = 0;
+		
+		return min;
+	}
+	
+	/**
+	 * 
+	 * @param label
+	 * @return the mean of all the valeur on this column
+	 */
+	public int moyenne(String label) {
+		int sum = 0;
+		
+		return sum;
+	}
+	
+	
+	
 }
---
+++ panda/src/test/java/devop_project/panda/DataFrameTest.java
@@ -0,0 +1,14 @@
+package devop_project.panda;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataFrameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
